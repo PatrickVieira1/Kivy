3a707257707bd225bb436ae261b3199a7b134455
unknown <patrickvieirae@gmail.com>
2021-09-20 21:39:46 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.widget import Widget
+from kivy.uix.textinput import TextInput
+from kivy.metrics import dp
+from kivy.lang import Builder
+from kivy.properties import NumericProperty, ObjectProperty
+from kivy.core.window import Window
+
+#Window.clearcolor = (1, 1, 1, 1)
+
+class MainWidget(BoxLayout):
+	pass
+
+class KivyTesteApp(App):
+	pass
+
+KivyTesteApp().run()
